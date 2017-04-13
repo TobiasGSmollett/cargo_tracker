6e944a0c11ec141995ca86594331c0cbf64c836f
tobias <landofscala@gmail.com>
2017-04-13 18:07:03 +0900

    Add voyage_number

--- src/cargo_tracker/domain/model/location/location.cr
+++ src/cargo_tracker/domain/model/location/location.cr
@@ -5,6 +5,10 @@ include CargoTracker::Domain::Shared
 
 module CargoTracker::Domain::Model::Location
 
+  #
+  # A location is our model is stops on a journey, such as cargo
+  # origin or destination, or carrier movement endpoints.
+  #
   class Location < Entity(UnLocode)
 
     getter id : UnLocode, name : String
---
+++ src/cargo_tracker/domain/model/voyage/voyage_number.cr
@@ -0,0 +1,25 @@
+require "../../shared/*"
+
+module CargoTracker::Domain::Model::Voyage
+
+  class VoyageNumber < ValueObject
+
+    getter number
+
+    def initialize(@number : String)
+    end
+
+    def ==(other : self)
+      number == other.number
+    end
+
+    def hash
+      number.hash
+    end
+
+    def to_s
+      number
+    end
+  end
+
+end
