1a98d8b79200c476a5a00715d3cb4243b57d5e62
tobias <s1190170@gmail.com>
2017-04-16 19:14:18 +0900

    Add HandlingEvent

---
+++ src/cargo_tracker/domain/model/handling/handling_event.cr
@@ -0,0 +1,38 @@
+require "./*"
+require "../voyage/*"
+require "../location/*"
+require "../cargo/*"
+require "../../shared/*"
+
+include CargoTracker::Domain::Shared
+include CargoTracker::Domain::Model::Cargo
+include CargoTracker::Domain::Model::Location
+include CargoTracker::Domain::Model::Voyage
+
+module CargoTracker::Domain::Model::Handling
+
+  class HandlingEvent < ValueObject
+
+    enum Type
+      LOAD = 1
+      UNLOAD = 1
+      RECEIVE = 0
+      CLAIM = 0
+      CUSTOMS = 0
+    end
+
+    getter cargo : Cargo
+    getter complete_time : Time
+    getter registration_time : Time
+    getter typ : Type
+    getter location : Location
+    getter voyage : (Voyage | Nil)
+            
+    def initialize(@cargo, @complete_time, @registration_time, @typ, @location, @voyage = nil)
+    end
+
+    def ==(other : self)
+      self == other
+    end
+  end
+end
