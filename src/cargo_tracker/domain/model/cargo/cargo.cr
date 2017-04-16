63d803781263f3ce761b44b1a3839f027532f6b6
tobias <s1190170@gmail.com>
2017-04-17 01:50:30 +0900

    Add some method to Delivery

--- src/cargo_tracker/domain/model/cargo/cargo.cr
+++ src/cargo_tracker/domain/model/cargo/cargo.cr
@@ -1,5 +1,7 @@
 require "./*"
+require "../location/*"
 require "../../shared/*"
+
 
 include CargoTracker::Domain::Shared
 
@@ -7,9 +9,38 @@ module CargoTracker::Domain::Model::Cargo
 
   class Cargo < Entity(TrackingId)
 
-    getter id
+    getter id : TrackingId
+    getter origin : Location
+    getter route_specification : RouteSpecification
+    getter itinerary : Itinerary
+    getter delivery : Delivery
 
-    def initialize(@id)
+    def initialize(@id, @route_specification : RouteSpecification)
+      @origin = route_specification.origin
+      @itinerary = Itinerary::EMPTY
+      @delivery = Delivery.derived_from @route_specification, @itinerary, HandlingHistory::EMPTY
+    end
+    
+    def specify_new_route(route_rpecification)
+      @route_specification = route_specification
+      @delivery = delivery.update_on_routing route_specification, itinerary
+    end
+    
+    def assign_to_route(itinerary)
+      @itinerary = itinerary
+      @delivery = delivery.update_on_routing route_specification, itinerary
+    end
+    
+    def derive_delivery_progress(handlingHistory)
+      @delivery = Delivery.derived_from route_specification, itinerary, handling_history
+    end
+    
+    def hash
+      id.hash
+    end
+    
+    def to_s
+      id.to_s
     end
   end
 end
